5895e41dd05af811edb13e8e71f27edf51e186ff
Tristan <tristangee@gmail.com>
2024-12-19 20:39:17 -0800

    redirect

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.use(expressLayouts);
 app.set('layout', 'layout'); // tells Express to use views/layout.ejs as the default layout
 const projects = require('./public/projects.json')
 
+app.use((req, res, next) => {
+    if (req.hostname === 'tristangee.com') {
+        return res.redirect(301, 'https://www.tristangee.com' + req.originalUrl);
+    }
+    next();
+});
+
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Home',
