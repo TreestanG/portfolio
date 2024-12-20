93bb982baeffb72600b587895a16dfb4963352fb
Tristan <tristangee@gmail.com>
2024-12-19 20:14:30 -0800

    add content

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const expressLayouts = require('express-ejs-layouts');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
+app.set('layout', 'layout'); // tells Express to use views/layout.ejs as the default layout
+const projects = require('./public/projects.json')
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home',
+    });
+});
+
+
+app.get('/projects', (req, res) => {
+    res.render('projects', {
+        title: 'Projects',
+        projects: projects.projects
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
